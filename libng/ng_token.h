304e0145aac3dc54329e4da3da2aa47e65ef2782
Randall Helzerman <rahelzer@yahoo.com>
2016-07-21 11:53:48 -0400

    wrapping up

--- libng/ng_token.h
+++ libng/ng_token.h
@@ -13,20 +13,21 @@ extern "C" {
     const char*       begin_;        // Beginning of token chars
     const char*       end_;          // One past ending of token chars
     long int          type_;         // type of token this is
+    char              airity_;       // how many arguments it takes
   } ng_token_t;
   
   
   // construction
   ng_token_t*
   ng_token_new(const char* begin,
-			   const char* end,
-			   const long int type);
+	       const char* end,
+	       const long int type);
   
   ng_token_t*
   ng_token_init(ng_token_t* self,
-			    const char* begin,
-			    const char* end,
-			    const long int type);
+		const char* begin,
+		const char* end,
+		const long int type);
   
   // destruction
   void ng_token_delete(ng_token_t** selfp);

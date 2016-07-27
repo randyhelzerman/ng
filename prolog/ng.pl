7f326d3de968f9a75b77f447522edbd51198facb
Randall Helzerman <rahelzer@yahoo.com>
2016-07-27 07:55:31 -0400

    strings work

--- prolog/ng.pl
+++ prolog/ng.pl
@@ -1,3 +1,5 @@
+:- discontiguous(make_it_stop/2).
+ 				  
 % Make a terminal expresion optional
 
 opt(_X) --> [].
@@ -5,7 +7,6 @@ opt(X)  --> X.
 
 opt(_X,_Y) --> [].
 opt(X,Y)   --> X,Y.
-
 
 star(_X) --> [].
 star(X) --> X, star(X).
@@ -25,25 +26,29 @@ string --> string_form(left_big_paren, right_big_paren).
 
 
 % Terminal
-terminal --> list(terminal_atom_q, ',').
+terminal --> list(terminal_atom_q, comma).
 
 terminal_atom_q --> terminal_atom, opt(quant).
-
+ 
 terminal_atom --> string.
 terminal_atom --> range.
 terminal_atom --> nonterminal.
 
+quant --> `*` .
+quant --> `+` .
+quant --> `?` .
 
-range --> '[', opt(range_neg), star(range_spec).
 
-range_neg --> ['^'].
+range --> `[`, opt(range_neg), star(range_spec), `]` .
+
+range_neg --> `^` .
 
 range_spec --> range_bound, opt(range_spec_ex).
 
-range_bound --> [X], {X \= '-', X \= ']', X \= ']', X \= ','}.
+range_bound --> [X], {[X] \= `-`, [X] \= `]`, [X] \= `]`, [X] \= `,`}.
 range_bound --> string.
 
-range_spec_ex --> [','], range_bound.
+range_spec_ex --> `,`, range_bound.
 
 id --> alpha, star(alphanumeric_).
 
@@ -51,42 +56,57 @@ alpha --> lowercase ; uppercase.
 
 alphanumeric_ --> alpha ; digit; [_].
 
-lowercase --> [X], 'a' =< X , X =< 'z'.
-uppercase --> [X], 'A' =< X,  X =< 'Z'.
-digit     --> [X], '0' =< X,  X =< '9'.
-
-param_args --> ['<'], list(id), ['>'].
-
-type_spec  --> [].
+lowercase --> [X], { [A]=`a`, [Z]=`z`,  A=<X,  X=<Z }.
+uppercase --> [X], { [A]=`A`, [Z]=`Z`,  A=<X,  X=<Z }.
+digit     --> [X], { [Z]=`0`, [N]=`9`,  Z=<X,  X=<N }.
 
 nonterminal --> id, opt(template_params), opt(type_spec).
 
+template_params --> `<`, list(id,comma), `>` .
+
+type_spec --> id.
+
+
 % testing
-a --> [a].
-b --> [b].
-comma --> [','].
-single_quote --> [39].
-double_quote --> ['\"'].
-left_big_paren --> ['-', '-', '['].
-right_big_paren --> [']', '-', '-'].
+a --> `a` .
+b --> `b` .
+	      
+comma            -->  `,`   .
+
+single_quote     -->  `'`   .   make_it_stop     -->  `'`  .
+double_quote     -->  `"`   .   make_it_stop     -->  `"`  .
+
+
+left_big_paren   -->  `--[` .
+right_big_paren  -->  `]--` .
+
 
 test :-
-    test_string.
+    test_string,
+    test_nonterminal.
 
 
 test_string :-
-    test_empty_string.
+    test_empty_string,
+    test_some_strings.
 
 
 test_empty_string :-
-    string_to_list("''", L),
-    string(L,[]),
+    string(`''`,[]),
+    
+    string(`""`,[]),
+    
+    string(`--[]--`,[]).
 
-    string_to_list([34,34],L1),
-    string(L1,[]),
 
-    string_to_lit("--[]--", L2),
-    string(L2,[]).
+	
+test_some_strings :-
+    string(`'a'`,[]),
+
+    string(`"'"`,[]),
+    
+    string(`--[yeah this is a lot of stuffs
+	      whihc just keeps going on]--`,[]).
 
 
 	
